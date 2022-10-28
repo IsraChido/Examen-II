649cac60de668d09d8cb369d8a0e48a234ca6438
IsraChido <2020030223@upsin.edu.mx>
2022-10-27 18:14:45 -0600

    Evento change

--- cambio.js
+++ cambio.js
@@ -1,16 +1,25 @@
 function change() {
+    let origen = document.getElementById('mOrigen');
+    let destino = document.getElementById('mDestino');
+    let cambio = "";
 
+    if(origen.value == 1) {
+        cambio = `<option value="6">Dólar estadounidense</option>
+        <option value="7">Dolar canadiense</option>
+        <option value="8">Euro</option>`
+    } else if (origen.value == 2) {
+        cambio = `<option value="5">Peso mexicano</option>
+        <option value="7">Dolar canadiense</option>
+        <option value="8">Euro</option>`
+    } else if (origen.value == 3) {
+        cambio = `<option value="5">Peso mexicano</option>
+        <option value="6">Dolar estadounidense</option>
+        <option value="8">Euro</option>`
+    } else if (origen.value == 4) {
+        cambio = `<option value="5">Peso mexicano</option>
+        <option value="6">Dolar estadounidense</option>
+        <option value="7">Dolar canadiense</option>`
+    }
+
+    destino.innerHTML = cambio;
 }
-
-function calcular() {
-    
-}
-
-function registro() {
-    
-}
-
-function borrar() {
-    
-}
-
